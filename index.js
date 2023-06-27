2669c011f6c9e5f6254181e04fabfd7756d9bc29
shmaphilo <philipogaye062@gmail.com>
2023-06-27 12:12:27 -0400

    completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+let num1=2
+let num2=31
+let multiply=num1*num2
+console.log(multiply)
 
-// Write your code here
+
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+ 
+
+let num3 = 22;
+let num4 = 7;
+let mod = (num3 % num4)+3;
+console.log(mod)
+
+
+
+
+
+let set = [10, 5, 20, 15, 8]; 
+
+let max = Math.max(...set);
